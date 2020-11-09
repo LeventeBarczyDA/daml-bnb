3bf82102dca562e8c578b835f05af4d542e6f2b6
David Padbury <david.padbury@digitalasset.com>
2020-11-09 11:37:22 -0600

    ui: support rate setting from UI

--- main/ui/src/components/InterestOfferDialog.tsx
+++ main/ui/src/components/InterestOfferDialog.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
 import { IndicationOfInterest, MakeAnOffer } from "@daml.js/damlbnb-0.0.1/lib/Rental";
-import { FormGroup, Container } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import { formatISO, parseISO } from 'date-fns'
 
 export interface InterestOfferDialogProps {
@@ -16,15 +15,11 @@ export interface InterestOfferDialogProps {
 }
 
 const InterestOfferDialog: React.FC<InterestOfferDialogProps> = ({ open, interestRequest, onClose }) => {
-    const [rate, setRate] = useState(250)
-
     const formatDateString = (date: string) => formatISO(parseISO(date), { representation: 'date' })
 
-    const buildRequest: () => MakeAnOffer = () => ({
-        rate: rate.toString()
-    })
+    const buildRequest: () => MakeAnOffer = () => ({})
 
-    const { tenant, start, end, address, currency } = interestRequest.payload
+    const { tenant, start, end, address } = interestRequest.payload
     const description = `${tenant} would like to stay between ${formatDateString(start)} and ${formatDateString(end)}.`
 
     return (
@@ -32,16 +27,6 @@ const InterestOfferDialog: React.FC<InterestOfferDialogProps> = ({ open, interes
             <DialogTitle>Request for {address}</DialogTitle>
             <DialogContent>
                 <Container>{description}</Container>
-                <Container style={{ paddingTop: 10 }}>
-                    <FormGroup>
-                        <TextField
-                            required
-                            label={`Rate (${currency})`}
-                            value={rate}
-                            type='number'
-                            onChange={e => setRate(parseFloat(e.target.value))} />
-                    </FormGroup>
-                </Container>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => onClose(null)} color="secondary">
--- main/ui/src/components/MyPropertyCard/MyPropertyCard.tsx
+++ main/ui/src/components/MyPropertyCard/MyPropertyCard.tsx
@@ -1,4 +1,4 @@
-import { IndicationOfInterest, MakeAnOffer, Property } from "@daml.js/damlbnb-0.0.1/lib/Rental"
+import { IndicationOfInterest, MakeAnOffer, Property, SetRate } from "@daml.js/damlbnb-0.0.1/lib/Rental"
 import Ledger from "@daml/ledger"
 import React, { useState } from "react"
 import PropertyCard from "../PropertyCard/PropertyCard"
@@ -7,6 +7,7 @@ import { useDablParties } from "../../common/parties";
 import { Mail } from "@material-ui/icons";
 import { IconButton } from "@material-ui/core";
 import InterestOfferDialog from "../InterestOfferDialog"
+import SetRateDialog from "../SetRateDialog";
 
 interface InterestRequestButtonProps {
     interestRequest: IndicationOfInterest.CreateEvent
@@ -22,6 +23,15 @@ const InterestRequestButton: React.FC<InterestRequestButtonProps> = ({ interestR
         setOpen(false)
     }
 
+    function onClick() {
+        // make sure a rate is set before attempting to make an offer
+        if (!property.payload.optRate) {
+            alert('Please set a rate first')
+        } else {
+            setOpen(true)
+        }
+    }
+
     return (
         <>
             <InterestOfferDialog
@@ -29,7 +39,7 @@ const InterestRequestButton: React.FC<InterestRequestButtonProps> = ({ interestR
                 interestRequest={interestRequest}
                 onClose={onClose}
             />
-            <IconButton onClick={() => setOpen(true)}>
+            <IconButton onClick={onClick}>
                 <Mail />
             </IconButton>
         </>
@@ -44,7 +54,10 @@ export interface MyPropertyCardProps {
 
 const MyPropertyCard: React.FC<MyPropertyCardProps> = ({ property, ledger, interestRequests }) => {
     const { parties } = useDablParties()
-    const isPublic = !!property.payload.optPublic
+    const { optPublic, optRate } = property.payload
+    const isPublic = !!optPublic
+    const rateText = optRate ? `${optRate._2} ${optRate._1}` : 'No rate set'
+    const [setRateDialogOpen, setSetRateDialogOpen] = useState(false)
 
     async function toggleVisibility() {
         if (isPublic) {
@@ -60,22 +73,46 @@ const MyPropertyCard: React.FC<MyPropertyCardProps> = ({ property, ledger, inter
         await ledger.exercise(IndicationOfInterest.MakeAnOffer, interestRequest.contractId, offer)
     }
 
+    async function setRateDialogClosed(request: SetRate | null) {
+        if (request) {
+            await ledger.exercise(Property.SetRate, property.contractId, request)
+        }
+        setSetRateDialogOpen(false)
+    }
+
     return (
         <>
+            <SetRateDialog
+                open={setRateDialogOpen}
+                onClose={setRateDialogClosed}
+                address={property.payload.address}
+                rateAmount={property.payload.optRate?._2 || null}
+                rateCurrency={property.payload.optRate?._1 || null}
+            />
             <PropertyCard property={property.payload}>
-                {interestRequests.map(interestRequest => (
-                    <InterestRequestButton
-                        key={interestRequest.contractId}
-                        interestRequest={interestRequest}
-                        property={property}
-                        onMakeOffer={offer => makeOffer(interestRequest, offer)}
-                    />
-                ))}
-                <Button
-                    color="primary"
-                    onClick={() => toggleVisibility()}
-                >{isPublic ? 'Make Private' : 'Make Public'}
-                </Button>
+                <div>
+                    {interestRequests.map(interestRequest => (
+                        <InterestRequestButton
+                            key={interestRequest.contractId}
+                            interestRequest={interestRequest}
+                            property={property}
+                            onMakeOffer={offer => makeOffer(interestRequest, offer)}
+                        />
+                    ))}
+                </div>
+                <div>
+                    <Button
+                        color="primary"
+                        onClick={() => setSetRateDialogOpen(true)}
+                    >
+                        {rateText}
+                    </Button>
+                    <Button
+                        color="primary"
+                        onClick={() => toggleVisibility()}
+                    >{isPublic ? 'Make Private' : 'Make Public'}
+                    </Button>
+                </div>
             </PropertyCard>
         </>
     )
